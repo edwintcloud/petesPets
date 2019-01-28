a300b43dc06156b1ada6a96cf2bd57379724c773
Edwin Cloud <ecloud412@gmail.com>
2019-01-28 14:06:40 -0800

    Added image upload to google cloud storage bucket

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,15 +1,16 @@
 if (document.querySelector("#new-pet")) {
   document.querySelector("#new-pet").addEventListener("submit", e => {
     e.preventDefault();
-
-    let pet = {};
-    const inputs = document.querySelectorAll(".form-control");
-    for (const input of inputs) {
-      pet[input.name] = input.value;
-    }
-
+    // Use FormData to grab everything now that we have files mixed in with text
+    var form = document.getElementById("new-pet");
+    var pet = new FormData(form);
+    // Assign the multipart/form-data headers to axios does a proper post
     axios
-      .post("/pets", pet)
+      .post("/pets", pet, {
+        headers: {
+          "Content-Type": "multipart/form-data;"
+        }
+      })
       .then(function(response) {
         window.location.replace(`/pets/${response.data.pet._id}`);
       })
@@ -20,9 +21,9 @@ if (document.querySelector("#new-pet")) {
           "Oops, something went wrong saving your pet. Please check your information and try again.";
         alert.style.display = "block";
         setTimeout(() => {
-          alert.style.display = 'none';
-          alert.classList.remove('alert-warning');
-         }, 3000)
+          alert.style.display = "none";
+          alert.classList.remove("alert-warning");
+        }, 3000);
       });
   });
 }
--- routes/pets.js
+++ routes/pets.js
@@ -1,5 +1,24 @@
-// MODELS
 const Pet = require("../models/pet");
+const multer = require("multer");
+const {Storage} = require('@google-cloud/storage');
+
+// Instantiate a storage client
+const googleCloudStorage = new Storage({
+  projectId: "petespets",
+  keyFilename: "storage_key.json"
+});
+
+// Multer is required to process file uploads and make them available via
+// req.files.
+const m = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // no larger than 5mb
+  }
+});
+
+// A bucket is a container for objects (files).
+const bucket = googleCloudStorage.bucket("petespets-bucket");
 
 // PET ROUTES
 module.exports = app => {
@@ -11,18 +30,52 @@ module.exports = app => {
   });
 
   // CREATE PET
-  app.post("/pets", (req, res) => {
-    var pet = new Pet(req.body);
+  app.post("/pets", m.single("avatar"), (req, res, next) => {
+    if (!req.file) {
+      res.status(400).send("No file uploaded.");
+      return;
+    }
 
-    pet
-      .save()
-      .then(pet => {
-        res.send({ pet: pet });
-      })
-      .catch(err => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
+    // Create a new blob in the bucket and upload the file data.
+    const blob = bucket.file(req.file.originalname);
+
+    // Make sure to set the contentType metadata for the browser to be able
+    // to render the image instead of downloading the file (default behavior)
+    const blobStream = blob.createWriteStream({
+      metadata: {
+        contentType: req.file.mimetype
+      }
+    });
+
+    blobStream.on("error", err => {
+      next(err);
+      return;
+    });
+
+    blobStream.on("finish", () => {
+      // The public URL can be used to directly access the file via HTTP.
+      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${
+        blob.name
+      }`;
+
+      // Make the image public to the web (since we'll be displaying it in browser)
+      blob.makePublic().then(() => {
+        req.body.avatarUrl = publicUrl;
+        var pet = new Pet(req.body);
+
+        pet
+          .save()
+          .then(pet => {
+            res.send({ pet: pet });
+          })
+          .catch(err => {
+            // STATUS OF 400 FOR VALIDATIONS
+            res.status(400).send(err.errors);
+          });
       });
+    });
+
+    blobStream.end(req.file.buffer);
   });
 
   // SHOW PET
