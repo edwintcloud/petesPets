79b02ba2b8b10c21137962ce33c360ac52fdbab2
Edwin Cloud <ecloud412@gmail.com>
2019-02-04 10:53:21 -0800

    Fix heroku deployment

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const methodOverride = require('method-override')
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/petes-pets');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/petes-pets');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
